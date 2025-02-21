70fbf44be4aa8de8666a2ab91460a0fab66d9e20
NathanBLsoftwares <nathan@blsoftwares.com.br>
2025-02-21 16:06:48 -0300

    Integração meia

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-let titulo = document.querySelector ("h1");
-titulo.innerHTML = "Jogo do número secreto"
+// let numeroAleatorio = numeroSecreto();
+
+// function exibirTextoNaTela(tag, texto){
+//     let campo =document.querySelector (tag);
+//     campo.innerHTML = texto;
+// }
+
+// exibirTextoNaTela ("h1", "Jogo do número secreto");
+// exibirTextoNaTela ("p", "Escolha um número entre 1 e 10");
+
+// function verificarChute(){
+//     console.log(numeroAleatorio);
+// }
+
+// function numeroSecreto() {
+//     return parseInt(Math.random() *10 + 1)
+// }
+
+function calcularDobro(numero){
+    return numero *2
+}
+let resultadoDobro = calcularDobro(4);
+console.log(resultadoDobro);
